19895bb0cf3b934137905c58882f8330e4c63db4
amanhex <aamankumar4958@gmail.com>
2024-07-29 12:17:42 +0530

    Plugin conf edit

---
+++ after/plugin/nvim-tree.lua
@@ -0,0 +1,6 @@
+require'nvim-tree'.setup {
+  -- other configurations...
+  update_cwd = true, -- Change the nvim-tree root directory to the new cwd
+  respect_buf_cwd = true, -- Respect the buffer's cwd
+}
+
